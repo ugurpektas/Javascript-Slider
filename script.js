01585ecd094e9ca5025d506601e4e960a358303f
UĞUR PEKTAŞ <76955236+ugurpektas@users.noreply.github.com>
2021-04-03 15:20:34 +0300

    butonlara slider kontrolleri eklendi

--- script.js
+++ script.js
@@ -27,7 +27,37 @@ var models = [
     }
 ];
 
-var index = 2;
+var index = 0;
+var slaytCount = models.length;
+
+showSlide(index);
+
+    //sol butonu
+document.querySelector('.fa-arrow-circle-left').addEventListener('click',function(){
+    index--;
+    showSlide(index);
+    console.log(index);
+});
+    //sağ butonu
+document.querySelector('.fa-arrow-circle-right').addEventListener('click',function(){
+    index++;
+    showSlide(index);
+    console.log(index);
+});
+
+
+
+function showSlide(i) {
+
+    index = i;
+
+    if (i<0) {
+        index = slaytCount - 1;
+    }
+    if (i >= slaytCount){
+        index = 0;
+    }
+
 
     //başlık isim
 document.querySelector('.card-title').textContent = models[index].name;
@@ -35,3 +65,4 @@ document.querySelector('.card-title').textContent = models[index].name;
 document.querySelector('.card-img-top').setAttribute('src',models[index].image);
     //link
 document.querySelector('.card-link').setAttribute('href',models[index].link);
+}
