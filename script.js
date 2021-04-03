8b7635c7444452c11e47d6514b8d4157e3b34119
UĞUR PEKTAŞ <76955236+ugurpektas@users.noreply.github.com>
2021-04-03 15:03:50 +0300

    sayfa tasarımı oluşturuldu ve slider atamaları yapıldı

---
+++ script.js
@@ -0,0 +1,37 @@
+    //araç bilgileri dizisi
+var models = [
+    {
+        name: 'Bmw 418d',
+        image: 'img/bmw.jpg',
+        link: 'https://www.arabam.com/ilan/galeriden-satilik-bmw-4-serisi-418i-gran-coupe-prestige/2016-bmw-418i-grand-coupe-prestige-harman-kardon-isitma-full/17292601'
+    },
+    {
+        name: 'Mazda CX-3',
+        image: 'img/mazda.jpg',
+        link: 'https://www.arabam.com/ilan/galeriden-satilik-mazda-cx-3-1-5-power/hatasiz-boyasiz-degisen-yok-ilk-elden-2015-model-mazda-cx-3/17273934'
+    },
+    {
+        name: 'Volvo S60',
+        image: 'img/volvo.jpg',
+        link: 'https://www.arabam.com/ilan/galeriden-satilik-volvo-s60-2-0-d-r-design/vip-motors-dan-degisensiz-volvo-s60-2-0-d-r-desing/17252772'
+    },
+    {
+        name: 'Skoda SUPER-B',
+        image: 'img/skoda.jpg',
+        link: 'https://www.arabam.com/ilan/galeriden-satilik-skoda-superb-1-6-tdi-style/2017-model-100-000-km-de-hatasiz-cam-tavanli-servis-bakimli-dizel-otomatik-superb/17255218'
+    },
+    {
+        name: 'Honda CIVIC',
+        image: 'img/honda.jpg',
+        link: 'https://www.arabam.com/ilan/galeriden-satilik-honda-civic-1-6-i-vtec-elegance/otomatik-sonruflu-bol-ekstrali-honda-civic-1-6-i-vtec-elegance-2008-model-diyarbakir/17158614'
+    }
+];
+
+var index = 2;
+
+    //başlık isim
+document.querySelector('.card-title').textContent = models[index].name;
+    //resim
+document.querySelector('.card-img-top').setAttribute('src',models[index].image);
+    //link
+document.querySelector('.card-link').setAttribute('href',models[index].link);
